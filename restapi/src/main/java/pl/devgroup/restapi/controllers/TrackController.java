bd6848db736bc5fae1740c49c8f6e5ab7af58d44
Mateusz <mateuszharazin@wp.pl>
2020-04-25 14:53:03 +0200

    Add new gitignore rules and rest method to select track details
    Signed-off-by: Mateusz <mateuszharazin@wp.pl>

--- restapi/src/main/java/pl/devgroup/restapi/controllers/TrackController.java
+++ restapi/src/main/java/pl/devgroup/restapi/controllers/TrackController.java
@@ -2,10 +2,13 @@ package pl.devgroup.restapi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import pl.devgroup.restapi.service.TrackService;
+
+import java.io.IOException;
 
 @Controller
 public class TrackController {
@@ -24,4 +27,13 @@ public class TrackController {
         modelAndView.setViewName("tracks");
         return modelAndView;
     }
+
+    @RequestMapping(value = "/getTrack/{trackId}", method = RequestMethod.GET)
+    public ModelAndView getTrack(@PathVariable("trackId") String trackId) throws IOException {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("trackDetails", trackService.getTrack(trackId));
+        modelAndView.setViewName("track");
+        return modelAndView;
+    }
+
 }
--- restapi/src/main/java/pl/devgroup/restapi/service/TrackService.java
+++ restapi/src/main/java/pl/devgroup/restapi/service/TrackService.java
@@ -1,16 +1,22 @@
 package pl.devgroup.restapi.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.devgroup.restapi.model.Track;
+import pl.devgroup.restapi.model.TrackDetails;
 import pl.devgroup.restapi.repository.TracksRepository;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class TrackService {
 
     private TracksRepository tracksRepository;
+    protected ObjectMapper mapper = new ObjectMapper();
+    private static final String RESOURCE_PATH = "src/main/resources/trackDetails/";
 
     @Autowired
     public TrackService(TracksRepository tracksRepository) {
@@ -19,6 +25,14 @@ public class TrackService {
 
     public List<Track> getTracks() {
         return tracksRepository.findAll();
+    }
+
+    public TrackDetails getTrack(String trackId) throws IOException {
+
+        Track track = tracksRepository.findByTrackId(trackId);
+        TrackDetails trackDetails = mapper.readValue(new File(RESOURCE_PATH + track.getTrackId() + ".json"), TrackDetails.class);
+
+        return trackDetails;
     }
 
 }
