80149dbdb5e715ded95d46afa079b2ca25044b24
Mateusz <mateuszharazin@wp.pl>
2020-04-26 20:10:18 +0200

    Improve HTML layout
    Signed-off-by: Mateusz <mateuszharazin@wp.pl>

--- restapi/src/main/java/pl/devgroup/restapi/controllers/UserController.java
+++ restapi/src/main/java/pl/devgroup/restapi/controllers/UserController.java
@@ -1,5 +1,6 @@
 package pl.devgroup.restapi.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +17,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public ModelAndView getHome() {
+    public ModelAndView getHome(Authentication authentication) {
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("activeUser", authentication.getName());
         modelAndView.setViewName("home");
         return modelAndView;
     }
